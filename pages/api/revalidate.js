52c7e5df713d2dcade11b8c16bfc368e9482a93a
unknown <dineshigdd@gmail.com>
2022-10-05 20:02:11 -0700

    add revaidation API

---
+++ pages/api/revalidate.js
@@ -0,0 +1,26 @@
+import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook';
+
+const handler = async (req, res) => {
+  try {
+    const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
+    if (
+      !isValidSignature(
+        JSON.stringify(req.body),
+        signature,
+        process.env.SANITY_WEBHOOK_SECRET
+      )
+    )
+      return res.status(401).json({ msg: 'Invalid request!' });
+
+    const { id } = req.body;
+    console.log( id)
+    await res.revalidate(`/products/`);
+    await res.revalidate(`/products/${id}`);
+    
+    res.status(200).json({ msg: 'Product pages revalidated.' });
+  } catch (error) {
+    res.status(500).json({ err: 'Something went Wrong!' });
+  }
+};
+
+export default handler;
